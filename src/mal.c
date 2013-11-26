e4c7fdb390ae2b319e8e6fa5760ff14e09f5043b
Nicholas Mazzuca <npmazzuca@gmail.com>
2013-11-25 23:18:25 -0800

    Changed license to CC0

--- src/mal.c
+++ src/mal.c
@@ -1,3 +1,17 @@
+/* 
+   MAL - Matrix Arithmetic Library
+
+   Written in 2013 by Mazzuca, Nicholas <npmazzuca@gmail.com>
+
+   To the extent possible under law, the author has dedicated all
+   copyright and related and neighboring rights to this software
+   to the public domain worldwide. This software is distributed
+   without any warranty.
+
+   You should have received a copy of the CC0 Public Domain
+   Dedication along with this software. If not, see
+   <http://creativecommons.org/publicdomain/zero/1.0/>
+*/
 #include <stdio.h>
 #include <malloc.h>
 #include "mal.h"
